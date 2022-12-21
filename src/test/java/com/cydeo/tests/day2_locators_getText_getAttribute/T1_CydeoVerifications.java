ef174e8a8e11ddcdb5772be3ed11e45e68f48fc3
Gulsah <gulsahingenc@gmail.com>
2022-12-21 20:04:14 +0000

    T1 added

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -0,0 +1,24 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_CydeoVerifications {
+
+    public static void main(String[] args) {
+
+
+        //TC #1: Cydeo practice tool verifications
+        //1. Open Chrome browser
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver =new ChromeDriver();
+        driver.manage().window().maximize();
+
+
+        //2. Go to https://practice.cydeo.com
+        //3.Verify URL contains
+        //Expected: cydeo
+
+    }
+}
