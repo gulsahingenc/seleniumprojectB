a7ef20f8641237e8783e62286ba76b77944974ce
Gulsah <gulsahingenc@gmail.com>
2022-12-21 23:13:15 +0000

    T2 third added

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_Linktext_Practice.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_Linktext_Practice.java
@@ -27,12 +27,31 @@ public class T2_Linktext_Practice {
         abTestLink.click();
 
 
-
         //4- Verify title is:
         //Expected: No A/B Test
+        String expectedTitle="No A/B Test";
+        String actualTitle=driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle)){
+            System.out.println("Title verification PASS!");
+        }else{
+            System.out.println("Title verification FAILED");
+        }
+
         //5- Go back to home page by using the .back();
+        driver.navigate().back();
+
+
         //6- Verify title equals:
         //Expected: Practice
+        String expectedTitle2="Practice";
+        actualTitle=driver.getTitle();
+
+        if(actualTitle.equals(expectedTitle2)){
+            System.out.println("Title verification PASS!");
+        }else{
+            System.out.println("Title verification FAILED!");
+        }
 
     }
 }
