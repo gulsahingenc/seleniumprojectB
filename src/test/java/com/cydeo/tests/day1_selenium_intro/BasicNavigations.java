0f13885bc4683a5084fa853e1bbda9e96ab2eaa9
Gulsah <gulsahingenc@gmail.com>
2022-12-21 15:36:02 +0000

    Basic Navigations

--- src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/BasicNavigations.java
@@ -16,8 +16,19 @@ public class BasicNavigations {
         //This is the line opening an empty browser
         WebDriver driver=new ChromeDriver();
 
+        //This line will maximize the browser size
+        driver.manage().window().maximize();
+
+
         //3-Go to "https://www.tesla.com"
         driver.get("https://www.tesla.com");
+
+        //get the title of the page
+        String currentTitle=driver.getTitle();
+        System.out.println("currentTitle = " + currentTitle);
+
+        String currentURL = driver.getCurrentUrl();
+        System.out.println("currentURL =" + currentURL);
 
         //Stop code execution for 3 seconds
         Thread.sleep(3000);
@@ -26,17 +37,45 @@ public class BasicNavigations {
         driver.navigate().back();
 
         //Stop code execution for 3 seconds
-        Thread.sleep(3000);
+       Thread.sleep(3000);
 
         //use selenium to navigate forward
-        driver.navigate().forward();
+       driver.navigate().forward();
 
         //Stop code execution for 3 seconds
-        Thread.sleep(3000);
+       Thread.sleep(3000);
 
 
         //use selenium to navigate refresh
-        driver.navigate().refresh();
+      driver.navigate().refresh();
+
+
+        //Stop code execution for 3 seconds
+       Thread.sleep(3000);
+
+        //use navigate().to():
+      driver.navigate().to("https://www.google.com");
+
+      //get the current title after getting the google page
+       currentTitle = driver.getTitle();
+
+
+        //System.out.println("driver.getTitle() ="+driver.getTitle());
+
+        System.out.println("currentTitle = " + currentTitle);
+
+        // Get the current URL using Selenium
+      currentURL = driver.getCurrentUrl();
+
+        System.out.println("currentURL = " + currentURL);
+
+
+
+        // this will close the currently opened window
+        driver.close();
+
+        // this will close all of the opened windows
+        driver.quit();
 
 
 
