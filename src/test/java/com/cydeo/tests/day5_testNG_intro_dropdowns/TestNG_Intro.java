26e709e2dccbccb487f6724d1255e92a6f37e35c
Gulsah <gulsahingenc@gmail.com>
2023-01-06 12:54:00 +0000

    T_NG added

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class TestNG_Intro {
@@ -23,14 +24,24 @@ public class TestNG_Intro {
         System.out.println("------> AfterMethod is running");
     }
 
-    @Test (priority = 2)
+    @Test (priority = 1)
     public void test1(){
         System.out.println("Test 1 is runnig..");
 
+        // ASSERT EQUALS: COMPARE 2 OF THE SAME THINGS
+
+        String actual="apple";
+        String expected="apple";
+        Assert.assertEquals(actual, expected);
+
     }
 
-    @Test (priority = 1)
+    @Test (priority = 2)
     public  void test2(){
         System.out.println("Test 2 is runnig..");
+
+        String actual="apple";
+        String expected="apple";
+        Assert.assertEquals(actual, expected);
     }
 }
